579cc4966965f819e297a764f23c4728742d6907
Kiana Alikhademi <kalikhademi@ufl.edu>
2019-03-30 18:20:56 -0400

    Kiana task 2 completed

--- KianaScript.py
+++ KianaScript.py
@@ -12,9 +12,9 @@ import numpy as np
 
 #====================== Reading the Data============================#
 
-Df = pd.read_table('new_data.txt')
+Df = pd.read_table('us_perm_visas.txt')
 
-#====================== Categorize employer size====================#
+#====================== Task 1====================#
 """according to https://data.oecd.org/entrepreneur/enterprises-by-business-size.htm the 0 to 10 employee is micro size enterprise, 11-49 Smalll, 
 50-249 medium, 250-inf large"""
 Df.head()
@@ -22,25 +22,16 @@ Df.replace('', -1, inplace=True)
 Df['employer_num_employees'].fillna(-1, inplace = True)
 
 Df['employer_num_employees'] = Df['employer_num_employees'].astype(int)
-
-#if(Df['employer_num_employees'] >0 and Df['employer_num_employees'] <50):
-#    Df['employer_num_employees'] = 'Small'
-#elif(Df['employer_num_employees'] >50 and Df['employer_num_employees'] <300):
-#    Df['employer_num_employees'] = 'Medium'
-#else:
-#    Df['employer_num_employees'] = 'Large'
+Df['employer_size']='Unknown'
 bins = [-1,0, 10, 49, 249,np.inf]
 names = ['Unknown', 'Micro', 'Small', 'Medium', 'Large']
 
-Df['employer_num_employees'] = pd.cut(Df['employer_num_employees'], bins, labels=names)
+Df['employer_size'] = pd.cut(Df['employer_num_employees'], bins, labels=names)
 Df.to_csv("data.txt", sep='\t')
 
-
-
-
-#=========================== mapping states into the office locations=======
+#=========================== Task 2 =======
 """this information has been gathered from this page"""
-Californa_center_states = ['Alaska','Arizona','California','Colorado','Commonwealth of the Northern Mariana Islands',
+California_center_states = ['Alaska','Arizona','California','Colorado','Commonwealth of the Northern Mariana Islands',
                     'Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana',
                     'Iowa', 'Kansas','Michigan','Minnesota','Missouri','Montana',
                     'Nebraska','Nevada','North Carolina','North Dakota','Ohio','Oregon',
@@ -52,11 +43,21 @@ Vermont_center_states =['Alabama', 'Arknsas', 'Connecticut', 'Delaware', 'Distri
                         'Puerto Rico','Rhode Island','South Carolina','Tennessee', 'U.S. Virgin Islands',
                         'Vermont','Virginia','West Virginia']
 
-california_centers_visa =['E-1','E-2','L','L-1A', 'L-1B','LZ','R-1' ]
+California_center_visa =['E-1','E-2','L','L-1A', 'L-1B','LZ','R-1' ]
 
-Vermont_centers_visa =['P','E-3','P-1A','P-1','TN-1','TN-2']
+Vermont_center_visa =['P','E-3','P-1A','P-1','TN-1','TN-2']
 Both_centers_visa = ['H-1B','H-1B2','H-1B','O-1A', 'O-1B','O-2',
                      'O','P-1B', 'P-1S', 'P-2', 'P-2S',' P-3',
                      'P-3S','Q-1']
 
+Df['Center'] = 'Unknown'
 
+
+VT_state_index = Df.loc[Df['employer_state'].isin(Vermont_center_states),'Center'] = 'VT'
+VT_visa_index = Df.loc[Df['class_of_admission'].isin(Vermont_center_visa),'Center'] ='VT'
+
+CA_state_index = Df.loc[Df['employer_state'].isin(California_center_states),'Center'] = 'CA'
+CA_visa_index = Df.loc[Df['class_of_admission'].isin(California_center_visa),'Center'] = 'CA'
+both_visa_index = Df.loc[Df['class_of_admission'].isin(Both_centers_visa),'Center'] ='CA or VT'
+
+#=================================== Task 3=======================
