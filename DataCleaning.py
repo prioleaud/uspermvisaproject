23981cd660b3cf5073807168cac841538b526680
Kiana Alikhademi <kalikhademi@ufl.edu>
2019-04-10 21:59:32 -0400

    Update DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -13,23 +13,18 @@ path = os.path.join(os.path.dirname(__file__), 'clean_data.csv')
 
 visa_data = pd.read_csv(path)
 
-
-
 """ use Kiana Code to do the tasks """
 
 visa_data['employer_size'] = EmployerRangeMapping(visa_data)
 visa_data['Center'] = StateMapping(visa_data)
 visa_data['Career_Cluster'] = MajorMapping(visa_data)
 
-# replace the maximum wage offer part
-#groups = visa_data.groupby(['wage_offer_unit_of_pay_9089'], as_index=False)[['wage_offer_from_9089']].max()
-# groups = visa_data.groupby('wage_offer_unit_of_pay_9089').wage_offer_from_9089.max()
-
-
-
-# print(groups)
 
 
 """ select the target variables and form the final data"""
 
+finalData = visa_data[['case_received_date','case_status','country_of_citizenship','decision_date','employer_state','employer_name','employer_yr_estab'
+						,'foreign_worker_info_birth_country','foreign_worker_info_education','job_info_alt_combo_ed', 'job_info_alt_occ_job_title', 'job_info_job_title',
+						'job_info_work_state','wage_offer_from_9089','wage_offer_unit_of_pay_9089','employer_size','Center','Career_Cluster' ]]
 
+finalData.to_csv('Final_data.csv')
